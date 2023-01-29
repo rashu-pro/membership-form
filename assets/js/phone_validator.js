0d7b70cfb8ac04ad57ec0b99325b64d2633f98b3
rashu-pro <rashu.pro@gmail.com>
2023-01-29 17:38:07 +0600

    ui issues has been fixed

--- assets/js/phone_validator.js
+++ assets/js/phone_validator.js
@@ -8,12 +8,12 @@ const alertMessageSelectorName = ".error-message";
 const userIpCookieName = "userCountry";
 
 //=== intl-tel-input plugin must be included before calling 'phoneNumberValidatior' function
-if(phoneNumberSelectors.length>0){
-    phoneNumberValidator(phoneNumberSelectors, phoneNumberWrapperSelectorName, alertMessageSelectorName);
-    //=== loading utils.js script and then giving country list div a custom width
-    window.intlTelInputGlobals.loadUtils("node_modules/intl-tel-input/build/js/utils.js").then(function () {
-        console.log('utils.js loaded!');
-    });
+if (phoneNumberSelectors.length > 0) {
+  phoneNumberValidator(phoneNumberSelectors, phoneNumberWrapperSelectorName, alertMessageSelectorName);
+  //=== loading utils.js script and then giving country list div a custom width
+  window.intlTelInputGlobals.loadUtils("node_modules/intl-tel-input/build/js/utils.js").then(function () {
+    console.log('utils.js loaded!');
+  });
 }
 
 /**
@@ -24,54 +24,55 @@ if(phoneNumberSelectors.length>0){
  *
  * @effects validate internation phone number
  */
-function phoneNumberValidator(phoneNumberSelectors, phoneNumberWrapperSelectorName, alertMessageSelectorName){
+function phoneNumberValidator(phoneNumberSelectors, phoneNumberWrapperSelectorName, alertMessageSelectorName) {
 
-    phoneNumberSelectors.forEach(phoneNumber=>{
-         if(phoneNumber.closest('.iti')) return;
+  phoneNumberSelectors.forEach(phoneNumber => {
+    if (phoneNumber.closest('.iti')) return;
 
-        const hiddenField = phoneNumber.closest(phoneNumberWrapperSelectorName).querySelector('input[type=hidden]');
-        let iti = window.intlTelInput(phoneNumber, {
-            allowDropdown: true,
-            initialCountry: "auto",
-            geoIpLookup: function(success, failure) {
-                let countryCode = 'US';
-                if(getCookie(userIpCookieName)){
-                    success(getCookie(userIpCookieName));
-                }else{
-                    $.get("https://ipinfo.io?token=bca72e9d426331", function() {}, "jsonp").always(function(resp) {
-                        countryCode = (resp && resp.country) ? resp.country : "us";
-                        success(countryCode);
-                        setCookie(userIpCookieName, countryCode, 10);
-                    });
-                }
-            },
-            //utilsScript: "assets/plugins/intl-tel-input/js/utils.js",
-            //onlyCountries: ["us", "cs", "pk", "bd", "in", "uk"]
-            preferredCountries: ["us", "cs", "pk", "bd", "in", "uk"],
-            separateDialCode: true,
-        });
-
-
-
-        if(phoneNumber.classList.contains('phone-number-edit')){
-            setNumber(iti, phoneNumber.getAttribute('data-number'));
+    const hiddenField = phoneNumber.closest(phoneNumberWrapperSelectorName).querySelector('input[type=hidden]');
+    let iti = window.intlTelInput(phoneNumber, {
+      allowDropdown: true,
+      initialCountry: "auto",
+      customPlaceholder: function (selectedCountryPlaceholder, selectedCountryData) {
+        return phoneNumber.getAttribute('placeholder')?phoneNumber.getAttribute('placeholder'):'';
+      },
+      geoIpLookup: function (success, failure) {
+        let countryCode = 'US';
+        if (getCookie(userIpCookieName)) {
+          success(getCookie(userIpCookieName));
+        } else {
+          $.get("https://ipinfo.io?token=bca72e9d426331", function () {
+          }, "jsonp").always(function (resp) {
+            countryCode = (resp && resp.country) ? resp.country : "us";
+            success(countryCode);
+            setCookie(userIpCookieName, countryCode, 10);
+          });
         }
-
-        phoneNumber.addEventListener('keyup',()=>{
-            errorHandling(iti, phoneNumber, phoneNumberWrapperSelectorName, alertMessageSelectorName, hiddenField);
-        });
-
-        phoneNumber.addEventListener('blur',()=>{
-            errorHandling(iti, phoneNumber, phoneNumberWrapperSelectorName, alertMessageSelectorName, hiddenField);
-        });
+      },
+      preferredCountries: ["us", "in", "cs", "uk", "pk", "bd"],
+      separateDialCode: true,
     });
 
-    let itiSelectedFlag = document.querySelectorAll('.iti__selected-flag');
-    itiSelectedFlag.forEach(selectedFlag=>{
-        selectedFlag.addEventListener('click', function () {
-            this.closest('.iti').querySelector('.iti__country-list').style.maxWidth = this.closest('.iti').clientWidth+"px";
-        });
+
+    if (phoneNumber.classList.contains('phone-number-edit')) {
+      setNumber(iti, phoneNumber.getAttribute('data-number'));
+    }
+
+    phoneNumber.addEventListener('keyup', () => {
+      errorHandling(iti, phoneNumber, phoneNumberWrapperSelectorName, alertMessageSelectorName, hiddenField);
     });
+
+    phoneNumber.addEventListener('blur', () => {
+      errorHandling(iti, phoneNumber, phoneNumberWrapperSelectorName, alertMessageSelectorName, hiddenField);
+    });
+  });
+
+  let itiSelectedFlag = document.querySelectorAll('.iti__selected-flag');
+  itiSelectedFlag.forEach(selectedFlag => {
+    selectedFlag.addEventListener('click', function () {
+      this.closest('.iti').querySelector('.iti__country-list').style.maxWidth = this.closest('.iti').clientWidth + "px";
+    });
+  });
 }
 
 /**
@@ -82,7 +83,7 @@ function phoneNumberValidator(phoneNumberSelectors, phoneNumberWrapperSelectorNa
  * @effects pre-set the number for input field
  */
 function setNumber(iti, number) {
-    iti.setNumber(number);
+  iti.setNumber(number);
 }
 
 /**
@@ -96,62 +97,62 @@ function setNumber(iti, number) {
  * @effects show/hide error message
  */
 function errorHandling(iti, phoneNumber, phoneNumberWrapperSelectorName, alertMessageSelectorName, hiddenField) {
-    let alertClass = '',
-        isvalidClass = '',
-        isFormGroupValid = '',
-        statusMessage = '',
-        showErrorMessage = 0;
+  let alertClass = '',
+    isvalidClass = '',
+    isFormGroupValid = '',
+    statusMessage = '',
+    showErrorMessage = 0;
 
-    let number = iti.getNumber(),
-        isValid = iti.isValidNumber(),
-        error = iti.getValidationError();
+  let number = iti.getNumber(),
+    isValid = iti.isValidNumber(),
+    error = iti.getValidationError();
 
-    if(!isValid && error === intlTelInputUtils.validationError.TOO_SHORT){
-        showErrorMessage = 1;
-        statusMessage = "Number is too short!";
-        alertClass = 'text-danger';
-        isvalidClass = 'invalid';
-    }
+  if (!isValid && error === intlTelInputUtils.validationError.TOO_SHORT) {
+    showErrorMessage = 1;
+    statusMessage = "Number is too short!";
+    alertClass = 'text-danger';
+    isvalidClass = 'invalid';
+  }
 
-    if(!isValid && error === intlTelInputUtils.validationError.TOO_LONG){
-        showErrorMessage = 1;
-        statusMessage = "Number is too long!";
-        alertClass = 'text-danger';
-        isvalidClass = 'invalid';
-    }
+  if (!isValid && error === intlTelInputUtils.validationError.TOO_LONG) {
+    showErrorMessage = 1;
+    statusMessage = "Number is too long!";
+    alertClass = 'text-danger';
+    isvalidClass = 'invalid';
+  }
 
-    if(!isValid && error === intlTelInputUtils.validationError.IS_POSSIBLE){
-        showErrorMessage = 1;
-        statusMessage = "Invalid number!";
-        alertClass = 'text-danger';
-        isvalidClass = 'invalid';
-    }
+  if (!isValid && error === intlTelInputUtils.validationError.IS_POSSIBLE) {
+    showErrorMessage = 1;
+    statusMessage = "Invalid number!";
+    alertClass = 'text-danger';
+    isvalidClass = 'invalid';
+  }
 
-    if(isValid && error === intlTelInputUtils.validationError.IS_POSSIBLE){
-        showErrorMessage = 1;
-        // statusMessage = "Number is valid!";
-        alertClass = 'text-success';
-        isvalidClass = 'valid';
-        isFormGroupValid = 'field-validated';
-    }
+  if (isValid && error === intlTelInputUtils.validationError.IS_POSSIBLE) {
+    showErrorMessage = 1;
+    // statusMessage = "Number is valid!";
+    alertClass = 'text-success';
+    isvalidClass = 'valid';
+    isFormGroupValid = 'field-validated';
+  }
 
-    if(phoneNumber.closest(phoneNumberWrapperSelectorName).querySelector(alertMessageSelectorName)){
-        phoneNumber.closest(phoneNumberWrapperSelectorName).querySelector(alertMessageSelectorName).remove();
-    }
+  if (phoneNumber.closest(phoneNumberWrapperSelectorName).querySelector(alertMessageSelectorName)) {
+    phoneNumber.closest(phoneNumberWrapperSelectorName).querySelector(alertMessageSelectorName).remove();
+  }
 
-    if(showErrorMessage){
-        let alertTag = document.createElement('p');
-        let classToadd = [alertClass, 'error-message'];
-        let classToRemove = ['valid', 'invalid', 'empty-field'];
-        alertTag.classList.add(...classToadd);
-        phoneNumber.classList.remove(...classToRemove);
-        alertTag.append(statusMessage);
-        phoneNumber.classList.add(isvalidClass);
-        if(isFormGroupValid!=='') phoneNumber.parentElement.parentElement.classList.add(isFormGroupValid);
-        phoneNumber.closest(phoneNumberWrapperSelectorName).append(alertTag);
-    }
+  if (showErrorMessage) {
+    let alertTag = document.createElement('p');
+    let classToadd = [alertClass, 'error-message'];
+    let classToRemove = ['valid', 'invalid', 'empty-field'];
+    alertTag.classList.add(...classToadd);
+    phoneNumber.classList.remove(...classToRemove);
+    alertTag.append(statusMessage);
+    phoneNumber.classList.add(isvalidClass);
+    if (isFormGroupValid !== '') phoneNumber.parentElement.parentElement.classList.add(isFormGroupValid);
+    phoneNumber.closest(phoneNumberWrapperSelectorName).append(alertTag);
+  }
 
-    hiddenField.value = number;
+  hiddenField.value = number;
 }
 
 /**
@@ -163,10 +164,10 @@ function errorHandling(iti, phoneNumber, phoneNumberWrapperSelectorName, alertMe
  * @effect does set a cookie in the browser
  */
 function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 /**
@@ -177,17 +178,17 @@ function setCookie(cname, cvalue, exdays) {
  * Get a specific cookie
  */
 function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
